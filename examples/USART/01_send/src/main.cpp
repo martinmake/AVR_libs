b42acab3106b0d3103c448ad33c9a94624187932
Martin Michalec <martin.michalec.2003@gmail.com>
2019-03-14 19:47:59 +0100

    common

---
+++ examples/USART/01_send/src/main.cpp
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <usart/usart.h>
+
+void init(void)
+{
+// 	Usart::INIT usart_init;
+// 	usart_init.tx              = Usart::TX::ON;
+// 	usart_init.character_size  = Usart::CHARACTER_SIZE::S8;
+// 	usart_init.stop_bit_select = Usart::STOP_BIT_SELECT::S2;
+// 	usart_init.baud            = BAUD;
+// 	usart_init.f_osc           = F_CPU;
+// 	Usart::begin(&usart_init);
+
+	Usart::begin(BAUD, F_CPU);
+}
+
+int main(void)
+{
+	init();
+
+	while (1) {
+		Usart::send_char('X');
+		Usart::send_char('\n');
+		Usart::send_str("TEST!");
+		Usart::send_char('\n');
+		Usart::sendf(10, "2 + 2 = %d", 2 + 2);
+		Usart::send_char('\n');
+		_delay_ms(1000);
+	}
+}
---
+++ examples/USART/02_recv/src/main.cpp
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <standard/standard.h>
+#include <usart/usart.h>
+
+#define LED_OFF '0'
+#define LED_ON  '1'
+
+Pin led({&PORTB, PB5});
+
+void init(void)
+{
+// 	Usart::INIT usart_init;
+// 	usart_init.tx              = Usart::RX::ON;
+// 	usart_init.character_size  = Usart::CHARACTER_SIZE::S8;
+// 	usart_init.stop_bit_select = Usart::STOP_BIT_SELECT::S2;
+// 	usart_init.baud            = BAUD;
+// 	usart_init.f_osc           = F_CPU;
+// 	Usart::begin(&usart_init);
+
+	Usart::begin(BAUD, F_CPU);
+	led.dd.set();
+}
+
+int main(void)
+{
+	init();
+
+	while (1) {
+		char command = Usart::recv_char();
+
+		switch (command) {
+			case LED_OFF: led.port.clear(); break;
+			case LED_ON:  led.port.set()  ; break;
+		}
+	}
+}
