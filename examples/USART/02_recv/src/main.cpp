e2ed84657359cc4b9667f35b6553238f136f8b25
Martin Michalec <martin.michalec.2003@gmail.com>
2019-04-23 18:06:34 +0200

    update

--- examples/USART/02_recv/src/main.cpp
+++ examples/USART/02_recv/src/main.cpp
@@ -1,38 +1,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <inttypes.h>
 
-#include <standard/standard.h>
-#include <usart/usart.h>
+#include <led/led.h>
+#include <usart/usart0.h>
 
-#define LED_OFF '0'
-#define LED_ON  '1'
+enum Command : char {
+	OFF = '0',
+	ON  = '1'
+};
 
-Pin led({PORTB, PB5});
-
-void init(void)
-{
-// 	Usart::INIT usart_init;
-// 	usart_init.tx              = Usart::RX::ON;
-// 	usart_init.character_size  = Usart::CHARACTER_SIZE::S8;
-// 	usart_init.stop_bit_select = Usart::STOP_BIT_SELECT::S2;
-// 	usart_init.baud            = BAUD;
-// 	usart_init.f_osc           = F_CPU;
-// 	Usart::begin(&usart_init);
-
-	Usart::begin(BAUD, F_CPU);
-	led.dd.set();
-}
+Led    led({PORTB, PB5});
+Usart0 usart0(TIO_BAUD, F_CPU);
 
 int main(void)
 {
-	init();
-
 	while (1) {
-		char command = Usart::recvc();
+		char command;
+		usart0 >> command;
 
 		switch (command) {
-			case LED_OFF: led.port.clear(); break;
-			case LED_ON:  led.port.set()  ; break;
+			case Command::OFF: led = 0; break;
+			case Command::ON:  led = 1; break;
 		}
 	}
 }
