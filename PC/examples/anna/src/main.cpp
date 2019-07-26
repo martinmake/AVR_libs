52fc82fe39c8810a19c28d4ada96d604b71f387a
Martin Michalec <martin.michalec.2003@gmail.com>
2019-07-26 20:45:29 +0200

    common

--- PC/examples/anna/src/main.cpp
+++ PC/examples/anna/src/main.cpp
@@ -13,28 +13,35 @@ int main(void)
 	nn.input_shape ({ 4, 1, 1 });
 	nn.output_shape({ 3, 1, 1 });
 
-	nn.add_layer("input");
-	nn.add_layer("full_connected", Shape(7, 1, 1));
-	nn.add_layer("hyperbolic_tangent");
-	nn.add_layer("output");
+	nn.add_layer("input"              /*  input   */);
+	nn.add_layer("full_connected",    Shape(7, 1, 1));
+	nn.add_layer("hyperbolic_tangent" /* constant */);
+	nn.add_layer("output"             /*  output  */);
 
 	/* ALTERNATIVE SYNTAX
-	nn << *new Layer::Input            ({ 4, 1, 1});
-	nn << *new Layer::FullConnected    ({ 7, 1, 1});
-	nn << *new Layer::HyperbolicTangent({ 7, 1, 1});
-	nn << *new Layer::Output           ({ 3, 1, 1});
+	nn << *new Layer::Input            (           );
+	nn << *new Layer::FullConnected    ({ 7, 1, 1 });
+	nn << *new Layer::HyperbolicTangent(           );
+	nn << *new Layer::FullConnected    (           );
+	*/
+	/* ALTERNATIVE SYNTAX
+	nn << *new Layer::Input            ({ 4, 1, 1 });
+	nn << *new Layer::FullConnected    ({ 7, 1, 1 });
+	nn << *new Layer::HyperbolicTangent({ 7, 1, 1 });
+	nn << *new Layer::FullConnected    ({ 3, 1, 1 });
 	*/
 
-	// nn.forward();
+	nn.hyperparameters().weight_generation_lower_limit(-0.01);
+	nn.hyperparameters().weight_generation_upper_limit(+0.01);
+	nn.hyperparameters().weight_generation_seed(time(NULL));
+	nn.generate_random_weights();
+
+//	nn.forward();
 
 //	nn.learning_rate(0.05);
 //	nn.momentum(0.01);
 //	nn.max_epochs(1000);
 //	nn.batch_size(10);
-
-//	nn.weight_generation_lower_limit(-0.01);
-//	nn.weight_generation_upper_limit(+0.01);
-//	nn.weight_generation_seed(time(NULL));
 
 //	nn.dataset(Dataset::Iris("datasets/iris"));
 //	nn.train();
