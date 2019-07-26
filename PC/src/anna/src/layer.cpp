602194901c875b2b273e2c083a0fc8d6f09912a9
Martin Michalec <martin.michalec.2003@gmail.com>
2019-07-26 22:18:36 +0200

    common

--- PC/src/anna/src/layer.cpp
+++ PC/src/anna/src/layer.cpp
@@ -11,10 +11,10 @@ namespace Anna
 	{
 		const std::map<const std::string, Layer::Data> datamap =
 		{
-			{ Input            ::NAME, { [](Shape shape = Shape(0, 0, 0)) { return new Input            (shape); }, Input            ::CHANGES_DATA_SHAPE, Input            ::IS_OUTPUT } },
-			{ Output           ::NAME, { [](Shape shape = Shape(0, 0, 0)) { return new Output           (shape); }, Output           ::CHANGES_DATA_SHAPE, Output           ::IS_OUTPUT } },
-			{ FullConnected    ::NAME, { [](Shape shape = Shape(0, 0, 0)) { return new FullConnected    (shape); }, FullConnected    ::CHANGES_DATA_SHAPE, FullConnected    ::IS_OUTPUT } },
-			{ HyperbolicTangent::NAME, { [](Shape shape = Shape(0, 0, 0)) { return new HyperbolicTangent(shape); }, HyperbolicTangent::CHANGES_DATA_SHAPE, HyperbolicTangent::IS_OUTPUT } },
+			{ Input            ::NAME, { [](Shape shape) { return new Input            (shape); }, Input            ::CHANGES_DATA_SHAPE, Input            ::IS_OUTPUT } },
+			{ Output           ::NAME, { [](Shape shape) { return new Output           (shape); }, Output           ::CHANGES_DATA_SHAPE, Output           ::IS_OUTPUT } },
+			{ FullConnected    ::NAME, { [](Shape shape) { return new FullConnected    (shape); }, FullConnected    ::CHANGES_DATA_SHAPE, FullConnected    ::IS_OUTPUT } },
+			{ HyperbolicTangent::NAME, { [](Shape shape) { return new HyperbolicTangent(shape); }, HyperbolicTangent::CHANGES_DATA_SHAPE, HyperbolicTangent::IS_OUTPUT } },
 		};
 
 		bool is_valid(const std::string& layer_name)
