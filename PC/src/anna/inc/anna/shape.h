401c50fb8f3c7334985775f9de4808fef5f9a436
Martin Michalec <martin.michalec.2003@gmail.com>
2019-07-27 14:06:50 +0200

    common

--- PC/src/anna/inc/anna/shape.h
+++ PC/src/anna/inc/anna/shape.h
@@ -12,10 +12,13 @@ namespace Anna
 			uint16_t m_width;
 			uint16_t m_height;
 			uint16_t m_channel_count;
+			uint16_t m_time;
+		public:
+			static const Shape INVALID;
 
 		public:
 			Shape(void);
-			Shape(uint16_t initial_width, uint16_t initial_height, uint16_t initial_channel_count);
+			Shape(uint16_t initial_width, uint16_t initial_height = 1, uint16_t initial_channel_count = 1, uint16_t initial_time = 1);
 			~Shape(void);
 
 		public:
@@ -25,24 +28,28 @@ namespace Anna
 			uint16_t width         (void) const;
 			uint16_t height        (void) const;
 			uint16_t channel_count (void) const;
+			uint16_t time          (void) const;
 		public: // SETTERS
 			void width         (uint16_t new_width         );
 			void height        (uint16_t new_height        );
 			void channel_count (uint16_t new_channel_count );
+			void time          (uint16_t new_time          );
 
 	};
 
-	inline bool Shape::is_valid(void) const { return m_width && m_height && m_channel_count; }
+	inline bool Shape::is_valid(void) const { return m_width && m_height && m_channel_count && m_time; }
 
 	// GETTERS
 	inline uint16_t Shape::width         (void) const { return m_width;          }
 	inline uint16_t Shape::height        (void) const { return m_height;         }
 	inline uint16_t Shape::channel_count (void) const { return m_channel_count;  }
+	inline uint16_t Shape::time          (void) const { return m_time;           }
 
 	// SETTERS
 	inline void Shape::width         (uint16_t new_width         ) { m_width          = new_width;          }
 	inline void Shape::height        (uint16_t new_height        ) { m_height         = new_height;         }
 	inline void Shape::channel_count (uint16_t new_channel_count ) { m_channel_count  = new_channel_count;  }
+	inline void Shape::time          (uint16_t new_time          ) { m_time           = new_time;           }
 }
 
 #endif
--- PC/src/anna/src/shape.cpp
+++ PC/src/anna/src/shape.cpp
@@ -2,13 +2,14 @@
 
 namespace Anna
 {
+	const Shape Shape::INVALID = Shape(0, 0, 0, 0);
 
 	Shape::Shape(void)
-		: Shape(0, 0, 0)
+		: Shape(INVALID)
 	{
 	}
-	Shape::Shape(uint16_t initial_width, uint16_t initial_height, uint16_t initial_channel_count)
-		: m_width(initial_width), m_height(initial_height), m_channel_count(initial_channel_count)
+	Shape::Shape(uint16_t initial_width, uint16_t initial_height, uint16_t initial_channel_count, uint16_t initial_time)
+		: m_width(initial_width), m_height(initial_height), m_channel_count(initial_channel_count), m_time(initial_time)
 	{
 	}
 
