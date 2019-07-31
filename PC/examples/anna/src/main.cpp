42dd975e91993ccf5b1185efeafad005096510f7
Martin Michalec <martin.michalec.2003@gmail.com>
2019-07-31 16:30:26 +0200

    common

--- PC/examples/anna/src/main.cpp
+++ PC/examples/anna/src/main.cpp
@@ -7,48 +7,47 @@
 #include <anna/cuda/device.cuh>
 #include <anna/layers/all.h>
 
+static Anna::Cuda::Device g_device(0);
+
 int main(void)
 {
 	using namespace Anna;
 
-	Cuda::Device device(0);
-	{
-		NeuralNetwork nn(device);
+	NeuralNetwork nn;
 
-		nn.input_shape ({ 4, 1, 1, 1 });
-		nn.output_shape({ 3, 1, 1, 1 });
+	nn.input_shape ({ 1, 1, 1, 4 });
+	nn.output_shape({ 1, 1, 1, 3 });
 
-	// 	/* SYNTAX
-	// 	nn.add_layer("input"              /*  input   */);
-	// 	nn.add_layer("full_connected",    Shape(7, 1, 1));
-	// 	nn.add_layer("hyperbolic_tangent" /* constant */);
-	// 	nn.add_layer("output"             /*  output  */);
-	// 	*/
-	//	/* ALTERNATIVE SYNTAX
-		nn << new Layer::Input            (          ); // optional
-	 	nn << new Layer::FullConnected    (Shape{ 7 });
-	 	nn << new Layer::HyperbolicTangent(          );
-		nn << new Layer::Output           (          );
-	//	*/
-		/* ALTERNATIVE SYNTAX
-		nn << new Layer::Input            ({ 4, 1, 1 });
-		nn << new Layer::FullConnected    ({ 7, 1, 1 });
-		nn << new Layer::HyperbolicTangent({ 7, 1, 1 });
-		nn << new Layer::FullConnected    ({ 3, 1, 1 });
-		*/
+// 	SYNTAX
+// 	nn.add_layer("input"              /*  input   */);
+// 	nn.add_layer("full_connected",    Shape(7, 1, 1));
+// 	nn.add_layer("hyperbolic_tangent" /* constant */);
+// 	nn.add_layer("output"             /*  output  */);
+// 	SYNTAX
+//	ALTERNATIVE SYNTAX
+	nn << new Layer::Input            (                ); // optional
+	nn << new Layer::FullConnected    (Shape{ 1, 1, 7 });
+	nn << new Layer::HyperbolicTangent(                );
+	nn << new Layer::Output           (                );
+//	ALTERNATIVE SYNTAX
+//	ALTERNATIVE SYNTAX
+//	nn << new Layer::Input            ({ 1, 1, 4 });
+//	nn << new Layer::FullConnected    ({ 1, 1, 7 });
+//	nn << new Layer::HyperbolicTangent({ 1, 1, 7 });
+//	nn << new Layer::FullConnected    ({ 1, 1, 3 });
+//	ALTERNATIVE SYNTAX
 
-		std::vector<Tensor> inputs         (10, nn.input_shape() );
-		std::vector<Tensor> desired_outputs(10, nn.output_shape());
+	std::vector<Tensor> inputs         (10, nn.input_shape() );
+	std::vector<Tensor> desired_outputs(10, nn.output_shape());
 
-		for (Tensor& input : inputs)
-			input.set_random(0, 10);
-		for (Tensor& desired_output : desired_outputs)
-			desired_output.set_random(0, 10);
+	for (Tensor& input : inputs)
+		input.set_random(0, 10);
+	for (Tensor& desired_output : desired_outputs)
+		desired_output.set_random(0, 10);
 
-		nn.hyperparameters().learning_rate(0.05);
-		nn.hyperparameters().batch_size(10);
+	nn.hyperparameters().learning_rate(0.05);
+	nn.hyperparameters().batch_size(5);
 
-	//	nn.dataset(Dataset::Iris("datasets/iris"));
-		nn.train(inputs, desired_outputs);
-	}
+//	nn.dataset(Dataset::Iris("datasets/iris"));
+	nn.train(inputs, desired_outputs, 10000);
 }
