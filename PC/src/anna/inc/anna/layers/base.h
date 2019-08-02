9f9f40a3b02eec5e307da5d333deb0434b654dfc
Martin Michalec <martin.michalec.2003@gmail.com>
2019-08-02 13:40:05 +0200

    common

--- PC/src/anna/inc/anna/layers/base.h
+++ PC/src/anna/inc/anna/layers/base.h
@@ -19,7 +19,7 @@ namespace Anna
 			protected: // MEMBER VARIABLES
 				std::shared_ptr<Hyperparameters> m_hyperparameters;
 				Shape                            m_shape;
-				Tensor                           m_input;
+				Shape                            m_input_shape;
 				Tensor                           m_output;
 				Tensor                           m_error;
 
@@ -33,30 +33,22 @@ namespace Anna
 				virtual void init(void);
 
 			public:
-				        void      forward(const Tensor& input);
-			protected:
-				virtual void cuda_forward(void);
-				virtual void  cpu_forward(void);
-
-			public:
-				        void      backward(Tensor& error_back, bool update_trainable_parameters);
-			protected:
-				virtual void cuda_backward(Tensor& error_back, bool update_trainable_parameters);
-				virtual void  cpu_backward(Tensor& error_back, bool update_trainable_parameters);
+				virtual void  forward(const Tensor& input);
+				virtual void backward(const Tensor& input, Tensor& error_back, bool update_trainable_parameters);
 
 			public: // GETTERS
 				        const Shape&   shape               (void) const;
-				        const Tensor&  input               (void) const;
+				        const Shape&   input_shape         (void) const;
 				        const Tensor&  output              (void) const;
 				        const Tensor&  error               (void) const;
 				              Tensor&  error               (void);
 				virtual       uint64_t trainable_parameters(void) const;
 				virtual       uint64_t flops               (void) const;
 			public: // SETTERS
-				void shape (const Shape&  new_shape );
-				void input (const Tensor& new_input );
-				void output(const Tensor& new_output);
-				void error (const Tensor& new_error );
+				void shape       (const Shape&  new_shape      );
+				void input_shape (const Shape&  new_input_shape);
+				void output      (const Tensor& new_output     );
+				void error       (const Tensor& new_error      );
 			public: // GETTERS FOR STATIC VARIABLES
 				virtual const std::string& name                    (void) const;
 				virtual       bool         changes_data_shape      (void) const;
@@ -65,25 +57,21 @@ namespace Anna
 				virtual       bool         has_trainable_parameters(void) const;
 		};
 
-		inline void Base::cuda_forward(void) { }
-		inline void Base::cpu_forward (void) { }
-
-		inline void Base::cuda_backward(Tensor& error_back, bool update_trainable_parameters) { (void) update_trainable_parameters; error_back = m_error; }
-		inline void Base:: cpu_backward(Tensor& error_back, bool update_trainable_parameters) { (void) update_trainable_parameters; error_back = m_error; }
+		inline void Base:: forward(const Tensor& input) { (void) input; }
+		inline void Base::backward(const Tensor& input, Tensor& error_back, bool update_trainable_parameters) { (void) input; (void) update_trainable_parameters; error_back = m_error; }
 
 		// GETTERS
 		inline const Shape&   Base::shape               (void) const { return m_shape;  }
-		inline const Tensor&  Base::input               (void) const { return m_input;  }
 		inline const Tensor&  Base::output              (void) const { return m_output; }
 		inline const Tensor&  Base::error               (void) const { return m_error;  }
 		inline       Tensor&  Base::error               (void)       { return m_error;  }
 		inline       uint64_t Base::trainable_parameters(void) const { return 0;        }
 		inline       uint64_t Base::flops               (void) const { return 0;        }
 		// SETTERS
-		inline void Base::shape (const Shape&  new_shape ) { m_shape  = new_shape;  }
-		inline void Base::input (const Tensor& new_input ) { m_input  = new_input;  }
-		inline void Base::output(const Tensor& new_output) { m_output = new_output; }
-		inline void Base::error (const Tensor& new_error ) { m_error  = new_error ; }
+		inline void Base::shape      (const Shape&  new_shape      ) { m_shape       = new_shape;       }
+		inline void Base::input_shape(const Shape&  new_input_shape) { m_input_shape = new_input_shape; }
+		inline void Base::output     (const Tensor& new_output     ) { m_output      = new_output;      }
+		inline void Base::error      (const Tensor& new_error      ) { m_error       = new_error ;      }
 
 		// GETTERS FOR STATIC VARIABLES
 		inline const std::string& Base::name                    (void) const { assert(false && "THIS IS JUST AN INTERFACE"); return *new std::string(); }
--- PC/src/anna/src/layers/base.cpp
+++ PC/src/anna/src/layers/base.cpp
@@ -18,12 +18,11 @@ namespace Anna
 
 		void Base::attach_to_neural_network(const Shape& initial_input_shape, const Shape& initial_shape, std::shared_ptr<Hyperparameters> initial_hyperparameters)
 		{
-			assert(      initial_shape.is_valid() ||         m_shape.is_valid());
-			assert(initial_input_shape.is_valid() || m_input.shape().is_valid());
+			assert(initial_shape.is_valid() || m_shape.is_valid());
 
-			if (      initial_shape.is_valid()) m_shape           = initial_shape;
-			if (initial_input_shape.is_valid()) m_input.shape(initial_input_shape);
-			if (true                          ) m_hyperparameters = initial_hyperparameters;
+			if (initial_shape.is_valid()) m_shape           = initial_shape;
+			if (true                    ) m_input_shape     = initial_input_shape;
+			if (true                    ) m_hyperparameters = initial_hyperparameters;
 
 			init();
 		}
@@ -32,26 +31,6 @@ namespace Anna
 		{
 			m_output.shape(m_shape);
 			m_error .shape(m_shape);
-		}
-
-		void Base::forward(const Tensor& input)
-		{
-			m_input = input;
-
-			#ifdef USE_CUDA
-				cuda_forward();
-			#else
-				cpu_forward();
-			#endif
-		}
-
-		void Base::backward(Tensor& error_back, bool update_trainable_parameters)
-		{
-			#ifdef USE_CUDA
-				cuda_backward(error_back, update_trainable_parameters);
-			#else
-				cpu_backward(error_back, update_trainable_parameters);
-			#endif
 		}
 	}
 }
