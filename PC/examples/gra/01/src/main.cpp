be6ad1b05c7dba5b57ab11e0558f4d78c4650e35
Martin Michalec <martin.michalec.2003@gmail.com>
2019-08-15 15:18:20 +0200

    common

--- PC/examples/gra/01/src/main.cpp
+++ PC/examples/gra/01/src/main.cpp
@@ -14,6 +14,7 @@
 int main(void)
 {
 	using namespace Gra;
+	using namespace GraphicsObject;
 
 	Renderer renderer(WINDOW_WIDTH, WINDOW_HEIGHT, "EXAMPLE WINDOW");
 
@@ -24,37 +25,36 @@ int main(void)
 		MODEL_WIDTH, MODEL_HEIGHT,
 			  0, MODEL_HEIGHT
 	};
-	VertexBuffer vertex_buffer(positions, 2 * 4 * sizeof(float));
+	Buffer::Vertex vertex_buffer(positions, 2 * 4 * sizeof(float));
 
-	VertexBufferLayout vertex_buffer_layout;
+	VertexArray::Layout vertex_buffer_layout;
 	vertex_buffer_layout.push<float>(2);
 
-	VertexArray vertex_array;
-	vertex_array.add_buffer(vertex_buffer, vertex_buffer_layout);
+	VertexArray vertex_array(vertex_buffer, vertex_buffer_layout);
 
-	unsigned int indices[2 * 3] =
+	std::vector<uint32_t> indices =
 	{
 		0, 1, 2,
 		2, 3, 0
 	};
-	IndexBuffer index_buffer(indices, 2 * 3);
+	Buffer::Index<uint32_t> index_buffer(indices);
 
-	Shader shader("res/shaders/basic");
+	Program program("res/shaders/basic");
 	{
 		glm::mat4 model      = glm::translate(glm::mat4(1.0), glm::vec3(MODEL_TRANSLATION_X, MODEL_TRANSLATION_Y, 0.0));
 		glm::mat4 view       = glm::translate(glm::mat4(1.0), glm::vec3(0.0,                 0.0,                 0.0));
 		glm::mat4 projection = glm::ortho(0.0, renderer.width() / ZOOM, 0.0, renderer.height() / ZOOM);
 		glm::mat4 mvp = projection * view * model;
 
-		shader.set_uniform_mat4f("u_mvp", mvp);
-		shader.set_uniform_4f("u_color", 0.7, 0.1, 0.5, 1.0);
+		program.set_uniform("u_mvp", mvp);
+		program.set_uniform("u_color", 0.7, 0.1, 0.5, 1.0);
 	}
 
 	while (!renderer.should_close())
 	{
 		renderer.start_frame();
 
-		renderer.draw(vertex_array, index_buffer, shader);
+		renderer.draw(vertex_array, index_buffer, program, DrawMode::TRIANGLES);
 
 		renderer.end_frame();
 	}
