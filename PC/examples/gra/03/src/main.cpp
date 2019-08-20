a46cca611438322d1f0b2384248305e7c2f50bb5
Martin Michalec <martin.michalec.2003@gmail.com>
2019-08-20 13:02:57 +0200

    common

--- PC/examples/gra/03/src/main.cpp
+++ PC/examples/gra/03/src/main.cpp
@@ -2,9 +2,6 @@
 
 #include <gra/renderer.h>
 
-#define WINDOW_WIDTH  640.0
-#define WINDOW_HEIGHT 420.0
-
 #define MODEL_WIDTH  100.0
 #define MODEL_HEIGHT 100.0
 
@@ -47,14 +44,14 @@ int main(void)
 
 		std::thread t1([&]()
 		{
-			Window rendering_window(WINDOW_WIDTH, WINDOW_HEIGHT, "RENDERING WINDOW", true);
+			Window window(640, 400, "RENDERING WINDOW", true);
 			Program program("res/shaders/basic");
 			VertexArray vertex_array(vertex_buffer, vertex_buffer_layout);
-			while (!rendering_window.should_close()) renderer.render(rendering_window, [&]()
+			while (!window.should_close()) renderer.render(window, [&]()
 			{
 				glm::mat4 model      = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 0.0, 0.0));
 				glm::mat4 view       = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 0.0, 0.0));
-				glm::mat4 projection = glm::ortho(0.0, WINDOW_WIDTH / ZOOM, 0.0, WINDOW_HEIGHT / ZOOM);
+				glm::mat4 projection = glm::ortho(0.0, window.width() / ZOOM, 0.0, window.height() / ZOOM);
 				glm::mat4 mvp = projection * view * model;
 				program.set_uniform("u_mvp", mvp);
 				program.set_uniform("u_color", 0.7, 0.1, 0.5, 1.0);
@@ -64,14 +61,14 @@ int main(void)
 
 		std::thread t2([&]()
 		{
-			Window rendering_window(WINDOW_WIDTH, WINDOW_HEIGHT, "RENDERING WINDOW", true);
+			Window window(400, 640, "RENDERING WINDOW", true);
 			Program program("res/shaders/basic");
 			VertexArray vertex_array(vertex_buffer, vertex_buffer_layout);
-			while (!rendering_window.should_close()) renderer.render(rendering_window, [&]()
+			while (!window.should_close()) renderer.render(window, [&]()
 			{
 				glm::mat4 model      = glm::translate(glm::mat4(1.0), glm::vec3(100.0, 100.0, 0.0));
 				glm::mat4 view       = glm::translate(glm::mat4(1.0), glm::vec3(  0.0,   0.0, 0.0));
-				glm::mat4 projection = glm::ortho(0.0, WINDOW_WIDTH / ZOOM, 0.0, WINDOW_HEIGHT / ZOOM);
+				glm::mat4 projection = glm::ortho(0.0, window.width() / ZOOM, 0.0, window.height() / ZOOM);
 				glm::mat4 mvp = projection * view * model;
 				program.set_uniform("u_mvp", mvp);
 				program.set_uniform("u_color", 0., 0.8, 0.9, 1.0);
