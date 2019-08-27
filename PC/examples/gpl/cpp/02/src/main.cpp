fe0ae35fe2044ccfe254ec43f5159df1fdd11320
Martin Michalec <martin.michalec.2003@gmail.com>
2019-08-27 15:10:48 +0200

    common

--- PC/examples/gpl/cpp/02/src/main.cpp
+++ PC/examples/gpl/cpp/02/src/main.cpp
@@ -8,11 +8,16 @@
 #define INNER_PADDING  2
 #define BLOCK_SIZE    ((CANVAS_WIDTH - OUTER_PADDING * 2) / 28)
 #define BUTTON_SIZE   (BLOCK_SIZE - INNER_PADDING * 2)
-#define POINT_COLOR   Color(0.5, 0.1, 1.0, 1.0)
+
+#define POINT_COLOR_BUTTON_PRESSED_LEFT  Color(0.3, 0.6, 0.9, 1.0)
+#define POINT_COLOR_BUTTON_PRESSED_RIGHT Color(0.5, 0.1, 1.0, 1.0)
+#define POINT_COLOR_INITIAL              POINT_COLOR_BUTTON_PRESSED_RIGHT
 
 int main(void)
 {
 	using namespace Gpl;
+	using namespace Gra;
+	using namespace Gra::Input::Window;
 	using namespace Gra::Math;
 
 	Canvas canvas(CANVAS_WIDTH, CANVAS_HEIGHT, CANVAS_TITLE);
@@ -20,12 +25,15 @@ int main(void)
 	for (uint8_t y = 0; y < 28; y++)
 	for (uint8_t x = 0; x < 28; x++)
 	{
-		Primitive::Shape::Point point(Position(x * BLOCK_SIZE + BLOCK_SIZE / 2, y * BLOCK_SIZE + BLOCK_SIZE / 2), POINT_COLOR, BUTTON_SIZE);
-	//	point.on_mouse_over([](auto& event)
-	//	{
-	//		std::cout << "[" << event.instance.position().x << ", " << event.instance.position().y << "] " << "ON MOUSE OVER" << std::endl;
-	//		event.is_handled(true);
-	//	});
+		Primitive::Shape::Point point(Position(x * BLOCK_SIZE + BLOCK_SIZE / 2, y * BLOCK_SIZE + BLOCK_SIZE / 2), POINT_COLOR_INITIAL, BUTTON_SIZE);
+		point.on_mouse_over([&](auto& event, void* instance)
+		{
+			Primitive::Shape::Point& point = *(Primitive::Shape::Point*) instance;
+			if      (canvas.mouse_button(Mouse::Button::LEFT) == Mouse::Action::PRESS)
+				point.color(POINT_COLOR_BUTTON_PRESSED_LEFT);
+			else if (canvas.mouse_button(Mouse::Button::RIGHT) == Mouse::Action::PRESS)
+				point.color(POINT_COLOR_BUTTON_PRESSED_RIGHT);
+		});
 		button_matrix << std::move(point);
 	}
 
--- PC/examples/gpl/cpp/03_colision_test/src/main.cpp
+++ PC/examples/gpl/cpp/03_colision_test/src/main.cpp
@@ -18,8 +18,14 @@ int main(void)
 
 	Canvas canvas(CANVAS_WIDTH, CANVAS_HEIGHT, CANVAS_TITLE);
 
-	Primitive::Shape::Point point    (Position(CONTAINER_WIDTH / 2, CONTAINER_HEIGHT / 2), POINT_COLOR, CONTAINER_WIDTH - INNER_PADDING * 2);
-	Primitive::Container    container(Position(OUTER_PADDING, OUTER_PADDING), Size(CONTAINER_WIDTH, CONTAINER_HEIGHT));
+	Primitive::Shape::Point point(Position(CONTAINER_WIDTH / 2, CONTAINER_HEIGHT / 2), POINT_COLOR, CONTAINER_WIDTH - INNER_PADDING * 2);
+	point.on_mouse_over([](auto& event, void* point_instance)
+	{
+		Primitive::Shape::Point& point = *(Primitive::Shape::Point*) point_instance;
+		point.color().w -= 0.001;
+	});
+
+	Primitive::Container container(Position(OUTER_PADDING, OUTER_PADDING), Size(CONTAINER_WIDTH, CONTAINER_HEIGHT));
 
 	container << std::move(point    );
 	canvas    << std::move(container);
