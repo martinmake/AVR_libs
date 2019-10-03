f97398c4592fe542f2c6e94bd14d3560e5eecd86
Martin Michalec <martin.michalec.2003@gmail.com>
2019-10-03 23:20:15 +0200

    common

--- AVR/examples/i2c/00_sandbox/src/main.cpp
+++ AVR/examples/i2c/00_sandbox/src/main.cpp
@@ -1,15 +1,15 @@
-#include <i2c/i2c.h>
+#include <i2c/software.h>
 #include <usart/usart0.h>
 
-#define SDA_PORT PORT::C
-#define SCL_PORT PORT::C
-#define SDA_PIN  1
-#define SCL_PIN  0
+#define SDA_PORT PORT::B
+#define SCL_PORT PORT::B
+#define SDA_PIN  0
+#define SCL_PIN  1
 
-// #define DEV_ADDRESS 0b0100111
-#define DEV_ADDRESS 0b0101001
+// #define DEVICE_ADDRESS 0b0100111
+#define DEVICE_ADDRESS 0b0101001
 
-I2c<SDA_PORT, SDA_PIN, SCL_PORT, SCL_PIN, 5> i2c;
+I2c::Software<SDA_PORT, SDA_PIN, SCL_PORT, SCL_PIN, 5> i2c;
 
 void init(void)
 {
@@ -23,11 +23,16 @@ void init(void)
 		bool ack = NACK;
 
 		i2c.start();
-		ack = i2c.write((DEV_ADDRESS << 1));
+		i2c.write((DEVICE_ADDRESS << 1));
+		ack = i2c.ack();
 		i2c.stop();
 
-		if (ack) break;
-		printf("DEVICE[0x%02X] NOT FOUND!\n", DEV_ADDRESS);
+		if (ack)
+		{
+			printf("DEVICE[0x%02X] FOUND!\n",     DEVICE_ADDRESS);
+			break;
+		}
+		else printf("DEVICE[0x%02X] NOT FOUND!\n", DEVICE_ADDRESS);
 	}
 }
 
@@ -38,15 +43,15 @@ int main(void)
 	while (true)
 	{
 		uint8_t data[3] = { 0 };
-		i2c.read_reg_8bit(DEV_ADDRESS, 0xC0, data, 3);
+		i2c.read_register_8bit(DEVICE_ADDRESS, 0xC0, data, 3);
 		printf("[0xC0] 0x%02X\n", data[0]);
 		printf("[0xC1] 0x%02X\n", data[1]);
 		printf("[0xC2] 0x%02X\n", data[2]);
-		printf("[0xC0] 0x%02X\n", i2c.read_reg_8bit (DEV_ADDRESS, 0xC0));
-		printf("[0xC1] 0x%02X\n", i2c.read_reg_8bit (DEV_ADDRESS, 0xC1));
-		printf("[0xC2] 0x%02X\n", i2c.read_reg_8bit (DEV_ADDRESS, 0xC2));
-		printf("[0x51] 0x%04X\n", i2c.read_reg_16bit(DEV_ADDRESS, 0x51));
-		printf("[0x61] 0x%04X\n", i2c.read_reg_16bit(DEV_ADDRESS, 0x61));
+		printf("[0xC0] 0x%02X\n", i2c.read_register_8bit (DEVICE_ADDRESS, 0xC0));
+		printf("[0xC1] 0x%02X\n", i2c.read_register_8bit (DEVICE_ADDRESS, 0xC1));
+		printf("[0xC2] 0x%02X\n", i2c.read_register_8bit (DEVICE_ADDRESS, 0xC2));
+		printf("[0x51] 0x%04X\n", i2c.read_register_16bit(DEVICE_ADDRESS, 0x51));
+		printf("[0x61] 0x%04X\n", i2c.read_register_16bit(DEVICE_ADDRESS, 0x61));
 		putchar('\n');
 		_delay_ms(10);
 	}
--- AVR/examples/i2c/01_scan/src/main.cpp
+++ AVR/examples/i2c/01_scan/src/main.cpp
@@ -1,17 +1,18 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-#include <standard/standard.h>
 #include <usart/usart0.h>
-#include <i2c/i2c.h>
+#include <i2c/software.h>
 
-#define F_SCL 9600
+#define SDA_PORT PORT::B
+#define SCL_PORT PORT::B
+#define SDA_PIN  0
+#define SCL_PIN  1
 
-Usart0 usart0(TIO_BAUD, F_CPU);
-I2c    i2c   (F_SCL   , F_CPU);
+I2c::Software<SDA_PORT, SDA_PIN, SCL_PORT, SCL_PIN, 5> i2c;
 
 void init(void)
 {
+	usart0.init({ TIO_BAUD });
+	stdout = usart0.stream();
+
 	sei();
 }
 
@@ -19,31 +20,34 @@ int main(void)
 {
 	init();
 
-	uint8_t value = 0x00;
+	usart0 << "[*] SCAN INITIATED!\n";
 
-	usart0 << "Scan initiated...\n";
+	bool at_least_one_device = false;
+	for (uint8_t addr = 0x04; addr <= 0x77; addr++) {
+		i2c.start();
+		i2c.write(addr << 1);
+		i2c.stop();
+
+		if (i2c.ack())
+		{
+			printf("[+] FOUND: 0x%02X\n", addr);
+			at_least_one_device = true;
+		}
+	}
 
 	for (uint8_t addr = 0x04; addr <= 0x77; addr++) {
-		i2c.read(addr, 1, &value);
-		i2c.wait_until_transceive_completed();
+		i2c.start();
+		i2c.write(addr << 1);
+		i2c.stop();
 
-		if (!i2c.transceive_failed)
-			usart0.sendf(20, "FOUND: 0x%02X\n", addr);
+		if (i2c.ack()) putchar('!');
+		else           putchar('.');
 	}
+	putchar('\n');
 
-	for (uint8_t addr = 0x04; addr <= 0x77; addr++) {
-		i2c.read(addr, 1, &value);
-		i2c.wait_until_transceive_completed();
+	usart0 << "[*] SCAN COMPLETED!\n";
+	if (!at_least_one_device)
+		usart0 << "[-] NO DEVICES WERE FOUND!\n";
 
-		if (!i2c.transceive_failed)
-			usart0 << '!';
-		else
-			usart0 << '.';
-	}
-	usart0 << '\n';
-
-	usart0 << "Scan completed...\n";
-
-	while (1) {
-	}
+	while (true) { }
 }
