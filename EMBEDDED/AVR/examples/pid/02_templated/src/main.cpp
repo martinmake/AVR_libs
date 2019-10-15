39c3102949fbd91ea928bed5d7008e9acb42ce7f
Martin Michalec <martin.michalec.2003@gmail.com>
2019-10-15 21:17:24 +0200

    common

---
+++ EMBEDDED/AVR/examples/pid/02_templated/src/main.cpp
@@ -0,0 +1,36 @@
+#include <adc.h>
+#include <math/pid/templated.h>
+#include <usart/usart0.h>
+
+using namespace Usart;
+
+#define KP 4
+#define KI 1
+#define KD 8
+#define INTEGRAL_LIMIT 100
+
+Pid::Templated<int16_t, int16_t, uint8_t, KP, KI, KD, INTEGRAL_LIMIT> pid;
+
+void init(void)
+{
+	usart0.init({ TIO_BAUD });
+	stdout = usart0.stream();
+
+	adc.init({ });
+	adc.channel(0);
+
+	sei();
+
+	adc.start_sampling();
+}
+
+int main(void)
+{
+	init();
+
+	while (true)
+	{
+		printf("DESIRED %+04d | ACTUAL %+04d | CONTROL %03u\n",
+			0, adc.value - 512, pid(-1 * (adc.value - 512)));
+	}
+}
--- EMBEDDED/AVR/src/math/inc/math/util.h
+++ EMBEDDED/AVR/src/math/inc/math/util.h
@@ -10,6 +10,19 @@ inline int16_t safe_add(int16_t lhs, int16_t rhs)
 	else                                  return lhs + rhs;
 }
 
+template <typename output_t, typename input_t>
+output_t safe_cast(input_t value)
+{
+	static_assert(false && sizeof(output_t), "TYPES NOT SUPPORTED");
+}
+template <>
+inline uint8_t safe_cast<uint8_t>(int16_t value)
+{
+	if (value > UINT8_MAX) return UINT8_MAX;
+	if (value <         0) return         0;
+	else                   return value;
+}
+
 template <typename T, T low, T high>
 T clamp(T value)
 {
@@ -17,7 +30,7 @@ T clamp(T value)
 	return (value < low) ? low : (value > high) ? high : value;
 }
 
-float clamp(float low, float high, float value)
+inline float clamp(float low, float high, float value)
 {
 	return (value < low) ? low : (value > high) ? high : value;
 }
